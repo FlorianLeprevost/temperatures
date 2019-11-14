ff3ebffff69288c2167e8b8125e0aae333dc884e
FlorianLeprevost <55445206+FlorianLeprevost@users.noreply.github.com>
2019-11-14 11:00:11 +0100

    added instructions how to use it if wrong arguments

--- Fahrenheit_celsius.py
+++ Fahrenheit_celsius.py
@@ -3,6 +3,15 @@
 
 
 import sys
+
+if len(sys.argv) != 3:
+	print("""Usage:
+		python Fahrenheit_celcius {F,C} temperature
+
+		converts C to F or F to C
+		""")
+	sys.exit(1)
+
 if sys.argv[1]=="F":
     temp = (int(sys.argv[2]) - 32)*5/9
     print(round(temp,2), " °C")
